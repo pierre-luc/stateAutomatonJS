ef2fca468b4c8f479dc9c7085e2ce1701873c846
Pierre-Luc <pierreluc.blot@gmail.com>
2015-11-13 19:25:51 +0100

    Création classe Arrow

---
+++ src/js/Arrow.src.js
@@ -0,0 +1,90 @@
+/**
+ * Classe Arrow
+ * @author Pierre-Luc BLOT
+ * @created 13/11/15
+ *
+ * @requires StateAutomaton.src.js
+ * @requires Line.src.js
+ * @requires HeadArrow.src.js
+ */
+(function(window){
+	'use strict';
+
+    /**
+     * @constructor
+     * Construit une flèche unidirectionnelle ou bidirectionnelle entre deux points déterminant les extrémités.
+     * @param param.start: Point
+     *  Point de départ de la flèche.
+     * @param param.end: Point
+     *  Point d'arrivé de la flèche.
+     * @param param.direction: string
+     *  'both' : bidirectionnelle
+     *  'left' : tête de flèche sur le point de départ
+     *  'right' : tête de flèche sur le point d'arrivé
+     *  'right' par défaut.
+     */
+	var Arrow = function( param ){
+		this.start = param.start;
+        this.end = param.end;
+
+        this.line = new stateAutomaton.graphic.Line({
+            start: this.start,
+            end: this.end
+        });
+
+        this.direction = 'right';
+        if ( typeof param.direction == "string" ){
+            this.direction = param.direction;
+        }
+
+        this.endHeadArrow = new stateAutomaton.graphic.HeadArrow({
+            origin: this.start,
+            angle: this.line.getAngle(),
+            height: 7,
+            width: 7
+        });
+    
+        this.startHeadArrow = new stateAutomaton.graphic.HeadArrow({
+            origin: this.end,
+            angle: this.line.getAngle() + Math.PI,
+            height: 7,
+            width: 7
+        });
+
+	};
+
+    /**
+     * Retourne le point de départ de la flèche.
+     * @return Point
+     */
+	Arrow.prototype.getStart = function(){
+		return this.start;
+	};
+
+    /**
+     * Retourne le point d'arrivé de la flèche.
+     * @return Point
+     */
+	Arrow.prototype.getEnd = function(){
+		return this.end;
+	};
+
+    /**
+     * Dessine la flèche dans le context2D.
+     * @param context:CanvasRenderingContext2D
+     *  Si context n'est pas pas définit, le context par défaut est chargé.
+     */
+	Arrow.prototype.draw = function( context ){
+		if ( typeof context === "undefined" ){
+    		context = window.stateAutomaton.graphic.defaultContext;
+    	}
+        this.line.draw( context );
+        if ( this.direction == 'right' || this.direction == 'both' ){
+            this.endHeadArrow.draw( context );
+        }
+        if ( this.direction == 'left' || this.direction == 'both' ){
+            this.startHeadArrow.draw( context );
+        }
+    };
+    window.stateAutomaton.graphic.Arrow = Arrow;
+})(window);
