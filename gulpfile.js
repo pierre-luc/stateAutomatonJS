d31bf5b4722d0ebe4df9bb101662113a376afe84
Pierre-Luc <pierreluc.blot@gmail.com>
2015-11-13 12:19:12 +0100

    Maj du gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,31 @@ var gulp      = require( 'gulp' ),
     sass      = require( 'gulp-sass' ),       // Conversion des SCSS en CSS
     minifyCss = require( 'gulp-minify-css' ), // Minification des CSS
     uglify    = require( 'gulp-uglify' ),     // Minification/Obfuscation des JS
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    resolveDependencies = require('gulp-resolve-dependencies'),
     plumber   = require( 'gulp-plumber' );    // Ne pas arrêter gulp en cas d'erreur
+
+gulp.task('js-linting-compiling', function(){
+  return gulp.src('./src/js/*.js') // read all of the files that are in script/lib with a .js extension
+    .pipe(resolveDependencies({
+      pattern: /\* @requires [\s-]*(.*\.js)/g
+    }))
+        .on('error', function(err) {
+            console.log(err.message);
+        })
+    .pipe(jshint()) // run their contents through jshint
+    .pipe(jshint.reporter('default')) // report any findings from jshint
+    .pipe(concat('StateAutomaton.js')) // concatenate all of the file contents into a file titled 'all.js'
+    .pipe(gulp.dest('./dist/js')) // write that file to the dist/js directory
+    .pipe(rename('StateAutomaton.min.js')) // now rename the file in memory to 'all.min.js'
+    .pipe(uglify({	// run uglify (for minification) on 'all.min.js'
+    	output: {
+    		comments: /^!|\b(copyright|license)\b|@(preserve|license|cc_on)\b/i
+    	}
+    }))
+    .pipe(gulp.dest('./dist/js')); // write all.min.js to the dist/js file
+});
 
 // SCSS TASK
 gulp.task( 'css', function() {
@@ -29,7 +53,8 @@ gulp.task( 'js-uglify', function() {
 // WATCH TASK
 gulp.task( 'watch', function() {
   gulp.watch( './src/style/sass/*.scss', [ 'css' ] );
-  gulp.watch( './src/js/*.src.js', [ 'js-uglify' ] );
+  //gulp.watch( './src/js/*.src.js', [ 'js-uglify' ] );
+  gulp.watch( './src/js/*.src.js', [ 'js-linting-compiling' ] );
 });
 
 gulp.task( 'default', [ 'watch' ] );
