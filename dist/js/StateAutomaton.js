e64498a033bc270ab26e67f061e14582f7e95490
Pierre-Luc <pierreluc.blot@gmail.com>
2015-11-17 01:08:29 +0100

    nouvelle distribution

--- dist/js/StateAutomaton.js
+++ dist/js/StateAutomaton.js
@@ -5,6 +5,7 @@
  */
 (function( window ){
     var stateAutomaton = {
+        pattern: {},
         graphic: {
             defaultContext: null,
             antialiasing: true,
@@ -60,7 +61,7 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  */
 (function(window) {
     'use strict';
@@ -130,7 +131,7 @@
  * @author Pierre-Luc BLOT
  * @created 16/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  */
 
  (function(window){
@@ -317,7 +318,7 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires Point.src.js
  * @requires Style.src.js
  */
@@ -428,12 +429,12 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires Point.src.js
  * @requires Line.src.js
  * @requires Style.src.js
  */
-
+ 
  (function(window){
     'use strict';
 
@@ -553,7 +554,7 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires Style.src.js
  */
 (function(window){
@@ -675,7 +676,7 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires HeadArrow.src.js
  * @requires Line.src.js
  * @requires Style.src.js
@@ -820,7 +821,7 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires Line.src.js
  * @requires HeadArrow.src.js
  * @requires Style.src.js
@@ -964,7 +965,7 @@
  * @author Pierre-Luc BLOT
  * @created 13/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires Point.src.js
  */
 
@@ -1036,7 +1037,7 @@
  * @author Pierre-Luc BLOT
  * @created 16/11/15
  *
- * @requires StateAutomaton.src.js
+ * @requires ../StateAutomaton.src.js
  * @requires Style.src.js
  */
 
@@ -1114,4 +1115,117 @@
     };
 
     window.stateAutomaton.graphic.Text = Text;
+ })(window);
+/**
+ * Classe Grid
+ * @author Pierre-Luc BLOT
+ * @created 16/11/15
+ *
+ * @requires ../StateAutomaton.src.js
+ * @requires ../graphic/Point.src.js
+ */
+
+ (function(window){
+    'use strict';
+
+    /**
+     * Construit une grille permettant d'y placer des éléments.
+     * @constructor
+     * @param param.row: number
+     * @param param.col: number
+     * @param param.width: number
+     * @param param.height: number
+     */
+    var Grid = function( param ) {
+        this.row = param.row;
+        this.col = param.col;
+        this.width = param.width;
+        this.height = param.height;
+
+        this.cell = {
+            width: this.width / this.col,
+            height: this.height / this.row
+        };
+    };
+
+    Grid.prototype.getSizeCell = function(){
+        return this.cell;
+    };
+
+    Grid.prototype.getRow = function(){
+        return this.row;
+    };
+
+    Grid.prototype.getCol = function(){
+        return this.col;
+    };
+
+    Grid.prototype.getHeight = function(){
+        return this.height;
+    };
+
+    Grid.prototype.getWidth = function(){
+        return this.width;
+    };
+
+    Grid.prototype.getCellAt = function( col, row ){
+        
+        function createPoint( x, y, name ){
+            return new stateAutomaton.graphic.Point({
+                coord: { x:x, y:y },
+                name: name ? name : ""
+            });
+        }
+
+        var cell = {
+            top: {
+                left: createPoint( col * this.cell.width, row * this.cell.height ),
+                center: createPoint( 
+                    ( col * this.cell.width + ( col + 1 ) * this.cell.width ) / 2,
+                    row * this.cell.height
+                ),
+                right: createPoint( ( col + 1 ) * this.cell.width, row * this.cell.height )
+            },
+            bottom: {
+                left: createPoint( col * this.cell.width, ( row + 1 ) * this.cell.height ),
+                center: createPoint( 
+                    ( col * this.cell.width + ( col + 1 ) * this.cell.width ) / 2,
+                    ( row + 1 ) * this.cell.height
+                ),
+                right: createPoint( ( col + 1 ) * this.cell.width, ( row + 1 ) * this.cell.height )
+            },
+            middle: createPoint( 
+                (col * this.cell.width + (col + 1) * this.cell.width) / 2,
+                (row * this.cell.height + (row + 1) * this.cell.height) / 2,
+                "(" + col + ", " + row + ")"
+            )
+        };
+        return cell;
+    };
+
+    Grid.prototype.getMatrix = function(){
+        var matrix = [];
+        for ( var col = 0; col < this.col; ++col ){
+            matrix[ col ] = [];
+            for ( var row = 0; row < this.row; ++row ){
+                matrix[ col ][ row ] = this.getCellAt( col, row );
+            }
+        }
+        return matrix;
+    };
+
+    Grid.prototype.drawCell = function( cell, context ){
+        if ( typeof context === "undefined" ){
+            context = window.stateAutomaton.graphic.defaultContext;
+        }
+        context.beginPath();
+        context.moveTo( cell.top.left.getCoord().x, cell.top.left.getCoord().y );
+        context.lineTo( cell.top.right.getCoord().x, cell.top.right.getCoord().y );
+        context.lineTo( cell.bottom.right.getCoord().x, cell.bottom.right.getCoord().y );
+        context.lineTo( cell.bottom.left.getCoord().x, cell.bottom.left.getCoord().y );
+        context.lineTo( cell.top.left.getCoord().x, cell.top.left.getCoord().y );
+        context.stroke();
+    }
+
+    window.stateAutomaton.pattern.Grid = Grid;
  })(window);
